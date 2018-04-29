c0b87943645f5579ce15ae9bd4fecdbdfbbc0cdf
chenpan@csii.com.cn <chenpan@csii.com.cn>
2018-04-29 16:34:03 +0800

    some bug

--- src/todo/item.vue
+++ src/todo/item.vue
@@ -1,5 +1,5 @@
 <template>
-	<div :class="['todo-item','todo.completed'?'completed':'']">
+	<div :class="['todo-item',todo.completed?'completed':'']">
 		<input
 			type="checkbox"
 			class="toggle"
@@ -26,17 +26,17 @@
 	}
 </script>
 <style lang="stylus" scoped>
-    .todo-item{
+    .todo-item {
         position relative
         background-color #fff
         font-size 24px
-        border-bottom 1px solid rgba(0,0,0,0.06)
-        &:hover{
-            .destroy:after{
+        border-bottom 1px solid rgba(0, 0, 0, 0.06)
+        &:hover {
+            .destory:after {
                 content: '×'
             }
         }
-        label{
+        label {
             white-space: pre-line;
             word-break: break-all;
             padding: 15px 60px 15px 15px;
@@ -45,35 +45,32 @@
             line-height: 1.2;
             transition: color 0.4s;
         }
-        &.completed{
-            label{
+        &.completed {
+            label {
                 color: #d9d9d9;
                 text-decoration line-through
             }
         }
     }
     .toggle{
-        text-align: center;
-        width: 40px;
-        height: 40px;
-        line-height: 40px
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        margin: 9px 0;
-        border: none;
-        appearance: none;
-        outline none
-        padding-left 5px
-        cursor pointer
-        &:after{
-            content: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI0MCIgaGVpZ2h0%0D%0APSI0MCIgdmlld0JveD0iLTEwIC0xOCAxMDAgMTM1Ij48Y2lyY2xlIGN4PSI1MCIgY3k9IjUwIiBy%0D%0APSI1MCIgZmlsbD0ibm9uZSIgc3Ryb2tlPSIjZWRlZGVkIiBzdHJva2Utd2lkdGg9IjMiLz48L3N2%0D%0AZz4=')
-        }
-        &:checked:after{
-            content: url('data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI0MCIgaGVpZ2h0%0D%0APSI0MCIgdmlld0JveD0iLTEwIC0xOCAxMDAgMTM1Ij48Y2lyY2xlIGN4PSI1MCIgY3k9IjUwIiBy%0D%0APSI1MCIgZmlsbD0ibm9uZSIgc3Ryb2tlPSIjYmRkYWQ1IiBzdHJva2Utd2lkdGg9IjMiLz48cGF0%0D%0AaCBmaWxsPSIjNWRjMmFmIiBkPSJNNzIgMjVMNDIgNzEgMjcgNTZsLTQgNCAyMCAyMCAzNC01Mnoi%0D%0ALz48L3N2Zz4=')
-        }
+      text-align: center;
+      width: 40px;
+      height: 40px;
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      margin: auto 0;
+      border: none;
+      appearance: none;
+      outline none
+      &:after{
+        content url('../assets/images/round.svg')
+      }
+      &:checked:after{
+        content url('../assets/images/done.svg')
+      }
     }
-    .destroy{
+    .destory {
         position: absolute;
         top: 0;
         right: 10px;
--- src/todo/todo.vue
+++ src/todo/todo.vue
@@ -64,24 +64,30 @@
 </script>
 
 <style lang="stylus" scoped>
-  .real-app
-    width 600px
-    margin 0 auto
-    box-shadow 0 0 5px #666
-  .add-input
-    position: relative
-    margin: 0
-    width: 100%
-    font-size: 24px
-    font-family: inherit
-    font-weight: inherit
-    line-height: 1.4em
-    border: 0
-    outline: none
-    color: inherit
-    box-sizing: border-box
-    font-smoothing: antialiased
-    padding: 16px 16px 16px 36px
-    border: none
-    box-shadow: inset 0 -2px 1px rgba(0, 0, 0, 0.03)
+    .real-app {
+        width 600px
+        margin 0 auto
+        box-shadow 0 0 5px #666
+    }
+
+    .add-input {
+        position: relative;
+        margin: 0;
+        width: 100%;
+        font-size: 24px;
+        font-family: inherit;
+        font-weight: inherit;
+        line-height: 1.4em;
+        border: 0;
+        outline: none;
+        color: inherit;
+        padding: 6px;
+        border: 1px solid #999;
+        box-shadow: inset 0 -1px 5px 0 rgba(0, 0, 0, 0.2);
+        box-sizing: border-box;
+        font-smoothing: antialiased;
+        padding: 16px 16px 16px 60px;
+        border: none;
+        box-shadow: inset 0 -2px 1px rgba(0, 0, 0, 0.03);
+    }
 </style>
